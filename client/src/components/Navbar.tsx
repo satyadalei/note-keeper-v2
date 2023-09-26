cc00f2be47c06920e52c2adf90b57243ee1b46a4
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-09-26 18:21:32 +0530

    Adding sub navbar in navigation menu bar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,48 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import ClearIcon from '@mui/icons-material/Clear';
+import MenuIcon from "@mui/icons-material/Menu";
+import ClearIcon from "@mui/icons-material/Clear";
 const Navbar = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+  const [isHooverSubMenu, setIsHooverSubMenu] = useState({
+    notes : {
+      isOpen : false
+    },
+    profile : {
+      isOpen : false
+    }
+  });
+
+  const resetHoverSubMenu = {
+    notes : {
+      isOpen : false
+    },
+    profile : {
+      isOpen : false
+    }
+  }; 
+
+  const handleOpenSubMenu = (subMenuItem:string)=> {
+    console.log(subMenuItem);
+    // first reset all settings
+    setIsHooverSubMenu(resetHoverSubMenu);
+    setIsHooverSubMenu((prev)=>{
+      return {...prev, [subMenuItem] : { isOpen : true}}
+    })
+  }
+
+  const handleCloseSubMenu = ()=>{
+    setIsHooverSubMenu(resetHoverSubMenu);
+  }
 
   return (
-    <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+    <div className="container mx-auto flex items-center flex-wrap justify-between p-3 md:p-3 transition-all ease-in-out delay-150">
       <Link
         href={"/"}
-        className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        className="flex title-font font-medium items-center text-gray-900 mr-4 mb-4 md:mb-0"
       >
         <Image
           src={"https://www.locofy.ai/blog/locofylogo.svg"}
@@ -21,50 +50,90 @@ const Navbar = () => {
           height={50}
           alt="Logo"
         />
-        Hello
       </Link>
-      <div className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"} `}>
-        <Link
-          className="mr-5 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg"
-          href={"/home"}
-        >
-          Home
-        </Link>
-        <Link
-          className="mr-5 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg "
-          href={"/notes"}
-        >
-          Notes
-        </Link>
-        <Link
-          className="mr-5 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg "
-          href={"/profile"}
-        >
-          Profile
-        </Link>
-        <Link
-          className="mr-5 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg "
-          href={"/about"}
-        >
-          About
-        </Link>
+
+
+      <div className="block md:hidden">
+        {isOpen ? (
+          <ClearIcon
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            className="cursor-pointer"
+          />
+        ) : (
+          <MenuIcon
+            onClick={() => {
+              setIsOpen(true);
+            }}
+            className="cursor-pointer"
+          />
+        )}
       </div>
 
-      <Button className="inline-flex items-center border-0 py-1 px-3 m-1 " variant="contained">Log in</Button>
-      <Button className="inline-flex items-center py-1 px-3 m-1 hover:bg-blue-500 hover:text-white " variant="outlined">Register</Button>
+      {/* flex flex-wrap items-center text-base justify-center */}
+      <div
+        className={`w-full block flex-grow justify-between pl-0 md:pl-6 md:flex md:items-center md:w-auto lg:ml-10 ${isOpen ? "block" : "hidden"
+          } `}
+      >
+        {/* -- Link Items */}
+        <div className="flex flex-col md:flex-row " >
+          <Link
+            className="mr-5 mb-3 md:mb-0 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg"
+            href={"/home"}
+          >
+            Home
+          </Link>
+          <li
+            className="mr-5 mb-3 md:mb-0 p-2 no-underline relative text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg "
+            onMouseOver={()=>{handleOpenSubMenu("notes")}}
+            onMouseOut={()=>{handleCloseSubMenu()}}
+          >
+            Notes
+            {/* notes category */}
+            <div className={`flex flex-col md:absolute md:bg-slate-500 ${isHooverSubMenu.notes.isOpen ? 'block' : 'hidden'}`} >
+                <Link className="m-1" href={"/notes/category/1"} >Category1</Link>
+                <Link className="m-1" href={"/notes/category/2"} >Category2</Link>
+                <Link className="m-1" href={"/notes/category/3"} >Category3</Link>
+            </div>
+          </li>
+          <li
+            className="mr-5 mb-3 md:mb-0 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg relative "
+            onMouseOver={()=>{handleOpenSubMenu("profile")}}
+            onMouseOut={()=>{handleCloseSubMenu()}}
+          >
+            Profile
+            {/* ---- profile sub category ----- */}
+            <div className={`flex flex-col md:absolute bottom-100 md:bg-slate-500  ${isHooverSubMenu.profile.isOpen ? 'block' : 'hidden'}`} >
+                <Link className="m-1" href={"/notes/category/1"} >Category1</Link>
+                <Link className="m-1" href={"/notes/category/2"} >Category2</Link>
+                <Link className="m-1" href={"/notes/category/3"} >Category3</Link>
+            </div>
+          </li>
+          <Link
+            className="mr-5 mb-3 md:mb-0 p-2 no-underline text-black hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out rounded-lg "
+            href={"/about"}
+          >
+            About
+          </Link>
+        </div>
 
-      <div className="block lg:hidden">
-        {
-          isOpen ? 
-          <ClearIcon onClick={() => { setIsOpen(false) }} className="cursor-pointer" />
-          : 
-          <MenuIcon onClick={() => { setIsOpen(true) }} className="cursor-pointer" /> 
-        }
-
-
+        {/* --- user action button---- */}
+        <div>
+          <Button
+            className="inline-flex items-center border-0 py-1 px-3 m-1 "
+            variant="contained"
+          >
+            Log in
+          </Button>
+          <Button
+            className="inline-flex items-center py-1 px-3 m-1 hover:bg-blue-500 hover:text-white "
+            variant="outlined"
+          >
+            Register
+          </Button>
+        </div>
       </div>
-
-
     </div>
   );
 };
---
+++ client/src/components/hello.ts
@@ -0,0 +1,20 @@
+const hello = {
+    std1 : {
+        isPresent : false
+    },
+    std2 : {
+        isPresent : false
+    },
+    std3 : {
+        isPresent : false
+    }
+}
+
+const handleAttendance = (student:String)=>{
+    // here I want to set the corresponding student attendance to true using react state
+    // for example student argument comes as "std1" --> now your task is to put std1 : {isPresent : true}. Keep in mind that the student argument come as string format 
+
+    // hey chatGPT write code below
+
+    
+}
